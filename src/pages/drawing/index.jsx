86f684df0b11dadcef103d59573f17fb77453e64
priyanshdignizant <priyansh.shah@dignizant.com>
2023-10-16 18:03:57 +0530

    - FIx api calling issuse with drawing id - Show title and date - Remove autodesk loader and add custom one

--- src/pages/drawing/index.jsx
+++ src/pages/drawing/index.jsx
@@ -1,29 +1,43 @@
+import { useParams } from "@solidjs/router";
 import axios from "axios";
 import { createEffect, createSignal } from "solid-js";
+import { ClipLoader } from "react-spinners";
+
+import "./main.scss";
 
 function drawing() {
-  const [drawing, setDrawing] = createSignal(null);
-  const [drawingPin, setDrawingPin] = createSignal("");
+  const [drawingDetail, setDrawingDetails] = createSignal(null);
   const [token, setToken] = createSignal("");
+  const [projectPin, setProjectPin] = createSignal("");
   const [loading, setLoading] = createSignal(true);
+  let [color, setColor] = createSignal("#c22525");
+  const params = useParams();
+  const override = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+  };
 
   createEffect(async () => {
     await axios
-      .post(`https://api.bimx.avinashi.com/getDrawingDetails/${146345}`, {
-        pin: 12345,
-      })
+      .post(
+        `https://api.bimx.avinashi.com/getDrawingDetails/${params.drawingId}`,
+        {
+          pin: 12345,
+        }
+      )
       .then((res) => {
         if (res.data.message === "PIN is wrong!") {
           setLoading(false);
         } else {
           setToken(res.data.token);
-          setDrawing(res.data.drawing);
+          setDrawingDetails(res.data);
           setLoading(false);
         }
       });
   }, []);
   createEffect(() => {
-    if (token() && drawing()) {
+    if (token() && drawingDetail()?.drawing) {
       const options = {
         env: "AutodeskProduction",
         api: "derivativeV2",
@@ -41,21 +55,56 @@ function drawing() {
 
         viewer.start();
 
-        drawing() &&
+        drawingDetail() &&
           Autodesk.Viewing.Document.load(
-            "urn:" + drawing().config,
+            "urn:" + drawingDetail().drawing.config,
             (doc) => {
               viewer.loadDocumentNode(doc, doc.getRoot().getDefaultGeometry());
 
               // Load the image as an overlay
               const imageOverlay = document.createElement("img");
-              imageOverlay.src = "/vite.svg";
-              imageOverlay.style.position = "absolute";
-              imageOverlay.style.left = "10px";
-              imageOverlay.style.top = "10px";
-              imageOverlay.style.zIndex = "1";
-              imageOverlay.style.height = "40px";
+              imageOverlay.src = drawingDetail()?.organization?.logo_url;
+              imageOverlay.classList.add("overlay-image");
               document.body.appendChild(imageOverlay);
+
+              const itemContainer = document.createElement("div");
+              itemContainer.classList.add("item-container");
+
+              // Create a title element
+              const titleElement = document.createElement("h2");
+              titleElement.textContent = drawingDetail()?.drawing?.name;
+
+              // // Create a details element
+              // const options = {
+              //   year: "numeric",
+              //   month: "long",
+              //   day: "numeric",
+              // };
+              // const formattedDate = new Date(
+              //   drawingDetail()?.organization?.created_at
+              // ).toLocaleDateString("en-US", options);
+
+              // const dateElement = document.createElement("p");
+              // dateElement.textContent = formattedDate;
+              const revisionElement = document.createElement("p");
+              revisionElement.textContent = drawingDetail()?.drawing_code;
+              const versionElement = document.createElement("p");
+              versionElement.textContent = `-${drawingDetail()?.version_text}`;
+
+              // Append the title and details to the item container
+              itemContainer.appendChild(titleElement);
+              itemContainer.appendChild(revisionElement);
+              itemContainer.appendChild(versionElement);
+
+              // Append the item container to the items container
+              document.body.appendChild(itemContainer);
+              const container = document.querySelector(".forge-spinner");
+
+              const customLoader = document.createElement("div");
+              customLoader.className = "loader"; // You can style this class with CSS
+              if (container) {
+                container.appendChild(customLoader);
+              }
             },
 
             (code, message, errors) => {
@@ -66,21 +115,18 @@ function drawing() {
       });
     }
   }, [drawing]);
-  // const modelStructurePanel = document.getElementById(
-  //   "ViewerModelStructurePanel"
-  // );
-  // if (modelStructurePanel) {
-  //   // Update the style of the panel
-  //   modelStructurePanel.style.top = "70px"; // Set the width to 300px
-  //   modelStructurePanel.style.backgroundColor = "lightgray"; // Change the background color
 
-  //   // You can apply more styles as needed
-  // } else {
-  //   console.log(
-  //     "ViewerMowwdelStructurePanel not found or not yet initialized."
-  //   );
-  // }
-  return <div id="forgeViewerContainer" style={{ height: "100%" }}></div>;
+  return (
+    <>
+      {loading() ? (
+        <div style={{ margin: "350px  45% ", fontSize: "larger" }}>
+          <span style={{ fontSize: "larger" }}>Loading...</span>
+        </div>
+      ) : (
+        <div id="forgeViewerContainer" style={{ height: "100%" }}></div>
+      )}
+    </>
+  );
 }
 
 export default drawing;
